1c7a82ab259eec513867a8e0194794f40b7a7fdf
danhaus <danhaus95@gmail.com>
2017-11-03 10:05:56 +0000

    LED library / tested

--- robotMain/robotMain.ino
+++ robotMain/robotMain.ino
@@ -1,9 +1,16 @@
+/*
+ * 
+ */
+
+#include "LED.h"
+
+// create objects
+LED myLED(13);
+
 void setup() {
-  // put your setup code here, to run once:
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  myLED.blink(1000);
 }
