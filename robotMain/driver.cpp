08d6b2ee74e0cfaa438ce3e5ff0253ac40149332
danhaus <danhaus95@gmail.com>
2017-11-09 13:32:30 +0000

    some vars renamed, structure cleared

--- robotMain/driver.cpp
+++ robotMain/driver.cpp
@@ -4,19 +4,21 @@
  */
 
 
+
 #include "driver.h" //include the declaration for this class
 #include "MD25.h"
 
 
-Driver::Driver(float Pp, float Pi, float Pd, int circumference, int wheel_dist) { // constructor, PID constants
+Driver::Driver(float Pp, float Pi, float Pd, int circumference, int wheel_dist, int limit_correction) { // constructor, PID constants
 	Kp = Pp;
 	Ki = Pi;
 	Kd = Pd;
 	cir = circumference;
 	w_dist = wheel_dist;
+	limit_cor = limit_correction;
 	error = 0;
 	previous_error = 0;
-	md = new MD25(0);
+	md = new MD25(0); // 0 is for mode 0 of MD25 and code is writ
 }
 
 int Driver::getEncVal(int dist) { // returns encoder value to be set to drive required distance
@@ -24,26 +26,31 @@ int Driver::getEncVal(int dist) { // returns encoder value to be set to drive re
 	return enc_count;
 }
 
-int Driver::getSpeed(int speed, int limit_correction) {
-	if (speed >= 255 - limit_correction) {
-		return (255 - limit_correction);
+int Driver::getSpeed(int speed) {
+	if (speed >= 255 - limit_cor) {
+		return (255 - limit_cor);
 	}
-	if (speed <= 0 + limit_correction) {
-		return (0 + limit_correction);
+	if (speed <= 0 + limit_cor) {
+		return (0 + limit_cor);
 	}
 	return speed;
 }
 
-void Driver::calculatePid(int enc_val, int target_val) {
-	error = target_val - enc_val;
+void Driver::calculatePid(int enc_val_cur, int target_val) {
+	error = target_val - enc_val_cur;
 	P = error;
 	I = I + error;
 	D = error - previous_error;
-	PID_value = ((Kp*P) + (Ki*I) + (Kd*D))/100;
+	PID_val = ((Kp*P) + (Ki*I) + (Kd*D))/100; // not sure about the sign after 128
+	PID_speed_theor = 128 +  PID_val;
+	PID_speed_limited = getSpeed(PID_speed_theor);
 }
 
 void Driver::forward(int dist) {
-	md->encReset();
+	md->encReset(); // reset encoders
+	int enc_target = getEncVal(dist); // get target value for encoders
+	int enc1 = md->encoder1(); // asign current value of encoder1 to var enc1
+	calculatePid(enc1, enc_target); // calculate PID value and assign it to private var PID_speed_limited
 }
 
 void Driver::printPid() {
@@ -55,8 +62,10 @@ void Driver::printPid() {
 	Serial.println(I);
 	Serial.print("D: ");
 	Serial.println(D);
-	Serial.print("PID_value: ");
-	Serial.println(PID_value);
+	Serial.print("PID_speed_theor: ");
+	Serial.println(PID_speed_theor);
+	Serial.print("PID_speed_limited: ");
+	Serial.println(PID_speed_limited);
 	Serial.println();
 }
 
--- robotMain/driver.h
+++ robotMain/driver.h
@@ -12,26 +12,27 @@
 
 class Driver {
 public:
-	Driver(float Pp, float Pi, float Pd, int circumference, int wheel_dist); // constructor
+	Driver(float Pp, float Pi, float Pd, int circumference, int wheel_dist, int limit_correction=15); // constructor
 	int getEncVal(int dist);
-	int getSpeed(int speed, int limit_correction=15); /* inputs speed from pid, if it is outside the limit
+	int getSpeed(int speed); /* inputs speed from pid, if it is outside the limit
 	[0+limit_correction, 255-limit_correction], returns the limit value, otherwise it returns the speed */
-	void calculatePid(int enc_val, int target_val); // calculates PID values
+	void calculatePid(int enc_val_cur, int target_val); // calculates PID values
 	void forward(int dist); // drives forward using PID and functions above
 	void printPid(); // prints current PID values and error
-	int encoder1;
-	int encoder2;
 
 private:
 	float Kp;
 	float Ki;
 	float Kd;
+	int limit_cor;
 	int error; // error for PID
 	int previous_error;
 	int P; // proportional error for PID
 	int I; // integral of error for PID
 	int D; // differential of error for PID
-	int PID_value; // value for PID (it is devided by 100 so that it does not require type float)
+	int PID_val; // value for PID (it is devided by 100 so that it does not require type float)
+	int PID_speed_theor; // PID_speed = 128 + PID_val (automatically asigned in calculatePID())
+	int PID_speed_limited; // limited PID speed so that the motor does not exceeds min and max speeds, performed by getSpeed(..)
 	int cir; // circumference of the wheel [mm]
 	int w_dist; // distance between wheels [mm]
 	MD25* md;
